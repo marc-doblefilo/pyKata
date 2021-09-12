35aa5f1f358a31c335b99a9e6b1a8ffadd7c9c29
jdes01 <jasampa3000@gmail.com>
2021-09-12 18:23:31 +0200

    feat: add pytest-sugar plugin

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update
 RUN apt-get upgrade -y
 RUN apt-get install -y python3 python3-pip python3-dev && pip install --upgrade pip
 RUN pip install -U pytest
+RUN pip install pytest-sugar
 
 RUN apt-get install -y nano
 
--- kata_file_test.py
+++ kata_file_test.py
@@ -6,3 +6,15 @@ class TestFuncion(unittest.TestCase):
         self.assertEqual(funcion(1),2)
         self.assertEqual(funcion(3),6)
         self.assertEqual(funcion(5),10)
+
+class TestFuncion1(unittest.TestCase):
+    def test_funcion(self):
+        self.assertEqual(funcion(1),2)
+        self.assertEqual(funcion(3),6)
+        self.assertEqual(funcion(5),20)
+
+class TestFuncion2(unittest.TestCase):
+    def test_funcion(self):
+        self.assertEqual(funcion(1),2)
+        self.assertEqual(funcion(3),6)
+        self.assertEqual(funcion(5),10)
